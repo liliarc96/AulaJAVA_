b2d01963d22b089078e49d29ec95c06444ff119e
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-10-14 20:10:18 -0300

    Update ExecutaPessoa.java

--- Pessoa/src/ExecutaPessoa.java
+++ Pessoa/src/ExecutaPessoa.java
@@ -4,6 +4,7 @@ public class ExecutaPessoa {
 	public static void main(String[] args) {
 		Pessoa pessoinha = new XMen();
 		PessoaEspecial pessoinha2 = new XMen();
+		//XMen wolverine = new Pessoa(); errado
 	}
 
 }
